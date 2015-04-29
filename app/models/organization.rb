40977de18f0f161b3c33a4ba383eb109f54760d6
Gaurub Pandey <gp@bajratechnologies.com>
2015-04-29 16:05:15 +0545

    Organization view + proper dev sqlite3 file

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,11 +3,11 @@ class Organization < ActiveRecord::Base
   has_many :contact_persons
 
   def self.show
-    ["name","location","phone"]
+    ["name","zone", "district"]
   end
 
   def self.show_details
-    ["name","location","phone"]
+    ["name","zone", "district"]
   end
 
 end
